23078e354d4d89c91ac215f98aa59084fe9cf404
Greg Doud <gpdoud@gmail.com>
2020-06-17 14:35:07 -0400

    Add project files.

---
+++ EvenOddResponse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace EvenOddResponse {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Enter an integer : ");
+            var ans = Console.ReadLine();
+            var nbr = Convert.ToInt32(ans);
+            if(nbr % 2 == 0) {
+                Console.WriteLine($"The number {nbr} is even");
+            } else {
+                Console.WriteLine($"The number {nbr} is odd");
+            }
+        }
+    }
+}
